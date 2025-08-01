ed26e7e62d6220a66b21b44031105c6bf370f9b6
adnansanaulla <adnansanaulla7860@gmail.com>
2025-08-01 02:23:02 -0400

    fetching stock data

--- fetch_stock_data.py
+++ fetch_stock_data.py
@@ -1,3 +1,19 @@
 import yfinance as yf
 import pandas as pd
 
+# 50 tickers
+tickers = ['AAPL', 'MSFT', 'GOOG', 'AMZN', 'TSLA', 'NVDA', 'META', 'NFLX', 'JPM', 'BAC',
+    'V', 'MA', 'UNH', 'HD', 'PG', 'DIS', 'PEP', 'KO', 'XOM', 'CVX',
+    'ADBE', 'CRM', 'CSCO', 'ORCL', 'INTC', 'T', 'WMT', 'BA', 'NKE', 'PFE',
+    'MRK', 'LLY', 'ABBV', 'MCD', 'IBM', 'GE', 'F', 'GM', 'QCOM', 'UPS',
+    'COST', 'SBUX', 'GS', 'DE', 'CAT', 'LMT', 'RTX', 'BKNG', 'BLK', 'MMM']
+
+dfs = []
+# loading all the data
+for ticker in tickers:
+    df = yf.download(ticker, start='2010-01-01', end='2025-01-01')
+    df['Ticker'] = ticker
+    dfs.append(df.reset_index())
+
+combined = pd.concat(dfs)
+combined.to_csv('stock_data.csv')
